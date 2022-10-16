8bd8a7b0a678719a632a74774884978f75ef9aa8
Samin Nazarpouri <nzsamin@yahoo.co.uk>
2022-10-16 22:35:42 +0100

    Add some small details

--- homework-1/cash_register.py
+++ homework-1/cash_register.py
@@ -17,38 +17,41 @@ For example, method add_item probably accepts some kind of an item?..
 
 class CashRegister:
 
-    def __init__(self, discount):
+    def __init__(self):
         self.total_items = dict() # {'item': 'price'}
         self.total_price = 0
-        self.discount = 0
 
     def add_item(self, item, price):
         self.total_items[item] = price
+        print(f"{item}s have been added to your basket. ")
         return self.total_items
 
     def remove_item(self, item):
         self.total_items.pop(item)
+        print(f"{item}s have been removed from your basket")
         return self.total_items
 
     def apply_discount(self, percentage):
         return self.total_price - (self.total_price * (percentage/100))
 
-
     def get_total(self):
         self.total_price = sum(float(self.total_items[item]) for item in self.total_items)
-        return self.total_price
+        print(f"The total price for your items in your basket is {self.total_price}")
 
     def show_items(self):
+        print("Here are all the items in your basket: ")
         for item in self.total_items:
             print(item)
 
     def reset_register(self):
         self.total_items.clear()
+        self.total_price = 0
+        print("Your basket has been emptied out! ")
 
 
 # EXAMPLE code run:
 
-groceries = CashRegister(discount=0.5)
+groceries = CashRegister()
 
 #ADD example:
 groceries.add_item('apple', '2.25')
@@ -61,15 +64,14 @@ groceries.add_item('celery', '2.50')
 groceries.remove_item('apple')
 
 # #Get total example:
-print(groceries.get_total())
+groceries.get_total()
 
-#Applying discount:
-total_price_before_discount= print(groceries.get_total())
+# #Applying discount:
+total_price_before_discount= groceries.get_total()
 print(groceries.apply_discount(50))
-
-# #Show items:
+#
+# # # #Show items:
 groceries.show_items()
-
-#Reset register:
+#
+# # #Reset register:
 groceries.reset_register()
-groceries.show_items()
