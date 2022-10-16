93c3a242ec5b0464bdc08cec7ba138a89f07af8c
Samin Nazarpouri <nzsamin@yahoo.co.uk>
2022-10-16 19:03:12 +0100

    Final work with examples

--- homework-1/student.py
+++ homework-1/student.py
@@ -14,14 +14,66 @@ Software Student and Data Science student.
 
 class Student:
 
-    def __init__(self, name, age, id):
+    def __init__(self, name, age, id, stream, university):
         self.name = name
         self.age = age
         self.id = id
         self.subjects = dict()
+        self.stream = stream
+        self.university = university
+
+    def add_subject(self, subject, grade):
+        self.subjects[subject] = grade
+        return self.subjects
+
+    def remove_subject(self, subject):
+        self.subjects.pop(subject)
+        return self.subjects
+
+    def show_all_subjects(self):
+        for subject in self.subjects:
+            print(subject)
+
+    def overall_mark(self):
+        sum_of_grades = sum(self.subjects[subject] for subject in self.subjects)
+        avg_grade = round(sum_of_grades / len(self.subjects), 2)
+        return avg_grade
+
+
 
 
 # class CFGStudent(<should inherit from Student>)
-#     create new methods that manage student's subjects (add/remove new subject and its graade to the dict)
+#     create new methods that manage student's subjects (add/remove new subject and its grade to the dict)
 #     create a method to view all subjects taken by a student
 #     create a method  (and a new variable) to get student's overall mark (use average)
+
+class CFGStudent(Student):
+
+    def __init__(self, name, age, id):
+        super().__init__(name=name, age=age, id=id, stream='software', university="CFG")
+
+
+
+first_student = CFGStudent(name='Sarah', age=20, id='1234')
+
+#Add subject
+first_student.add_subject('Python foundation', 60)
+first_student.add_subject('Theory foundation', 75)
+first_student.add_subject('Algorithms', 84)
+first_student.add_subject('Databases', 65)
+first_student.show_all_subjects()
+
+#Remove subject
+first_student.remove_subject('Python foundation')
+first_student.show_all_subjects()
+
+#Overall mark
+print(first_student.overall_mark())
+
+
+
+
+
+
+
+
